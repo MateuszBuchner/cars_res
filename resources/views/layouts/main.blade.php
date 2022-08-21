93463b26bdcfebbb0014fb22469c27754ae69bea
MateuszBuchner <mateusz.buchner369@gmail.com>
2022-08-21 14:01:11 +0200

    readme

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Cars</title>
     <title>Cars</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
